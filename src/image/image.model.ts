77c54eb2c5d8ccc7a010910a3c95295779583862
Ievgen Khoptiar <evgeni.khoptiar@emg-soft.com>
2020-11-02 03:20:53 +0200

    fix for node version 14+ implicity errors and mixed types

--- src/image/image.model.ts
+++ src/image/image.model.ts
@@ -1,22 +1,31 @@
-import { prop } from '@typegoose/typegoose';
+import { modelOptions, prop, Severity } from '@typegoose/typegoose';
 import { Schema } from 'mongoose';
+
+type ImageFile = {
+    data: Buffer | null;
+    contentType: string | null;
+}
+
+@modelOptions({
+    options: {
+        allowMixed: Severity.ALLOW,
+        customName: 'notification',
+    },
+})
 
 export class ApiImage {
     readonly _id: Schema.Types.ObjectId;
 
-    @prop({required: true})
+    @prop ( { required: true } )
     name: string;
 
-    @prop()
+    @prop ()
     type: string;
 
-    @prop({ default: { data: null, contentType: null } })
-    image_file: {
-        data: Buffer;
-        contentType: string;
-    }
+    @prop ( { default: { data: null, contentType: null } } )
+    image_file: ImageFile
 
-    @prop({default: Date.now() })
+    @prop ( { default: Date.now () } )
     createdAt: Date;
 
     url: string;
