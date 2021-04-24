5df1ca372a5a2d0505b85313a82cbef86b9cd3b8
Evgeni Khoptiar <evgeni.khoptiar@emg-soft.com>
2021-04-24 18:27:02 +0300

    db to process.env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,16 @@
-import { Module } from '@nestjs/common';
-import { ImageModule } from './image/image.module';
-import { TypegooseModule } from "nestjs-typegoose";
+import {Module} from '@nestjs/common';
+import {ImageModule} from './image/image.module';
+import {TypegooseModule} from "nestjs-typegoose";
 
 @Module({
-  imports: [ImageModule,
-    TypegooseModule.forRoot('mongodb+srv://ardname:kiev220976@cluster0.5vf6a.mongodb.net/imagesapi?retryWrites=true&w=majority', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
-  ],
-  controllers: [],
-  providers: [],
+    imports: [ImageModule,
+        TypegooseModule.forRoot(process.env.DB_LINK + '?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+    ],
+    controllers: [],
+    providers: [],
 })
-export class AppModule {}
+export class AppModule {
+}
